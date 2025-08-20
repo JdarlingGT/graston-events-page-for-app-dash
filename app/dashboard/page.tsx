14989f62c7181db43badc439f34e0082342b2ef4
yesmannow <hoosierdarling@gmail.com>
2025-08-20 03:55:57 -0400

    Committing all changes before push

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideNav from '../../ui/dashboard/sidenav';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div className="dashboard-layout">
+      <SideNav />
+      <div className="dashboard-content">
+        {/* Add other dashboard components here */}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
