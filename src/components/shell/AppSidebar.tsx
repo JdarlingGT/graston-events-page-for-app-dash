3cf496fa417140d6b7ee2578929efc8b714d4b0a
yesmannow <hoosierdarling@gmail.com>
2025-08-20 00:12:00 -0400

    chore: push latest changes

---
+++ src/components/shell/AppSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppSidebar: React.FC = () => {
+  return (
+    <div className="app-sidebar">
+      {/* Sidebar content goes here */}
+    </div>
+  );
+};
+
+export default AppSidebar;
---
+++ src/components/shell/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topbar: React.FC = () => {
+  return (
+    <div className="topbar">
+      {/* Topbar content goes here */}
+    </div>
+  );
+};
+
+export default Topbar;
